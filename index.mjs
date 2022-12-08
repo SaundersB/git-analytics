de93c486c8c4c469c200988bb23352b7db3be0d7
Brandon Saunders <brandon.saunders@charlesriverlabs.com>
2022-12-08 15:56:06 -0800

    feat: import commits

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv'
 dotenv.config();
 import { getAzureDevOpsProvider } from './git-tool-providers/azure-dev-ops.mjs';
 import { openDb } from './db.mjs';
+import { importCommits } from './importData.mjs';
 
 let orgUrl = process.env.ORG_URL
 let token = process.env.AZURE_PERSONAL_ACCESS_TOKEN
@@ -15,20 +16,5 @@ console.log(commits);
 
 // const pullRequests = await gitApiObject.getPullRequests(repoId, {}, project);
 // console.log(pullRequests);        
-
-
 const db = await openDb();
-console.log(`Importing ${commits.length} commits`)
-commits.map(async (commit) => {
-    const author = await db.get('SELECT * FROM authors WHERE email = ?;', commit.committer.email);
-    console.log(`author`, author)
-
-    if(!author) {
-        await db.run(
-            'INSERT INTO authors (name, email, createDate) VALUES (?, ?, ?);', 
-            commit.committer.name, 
-            commit.committer.email, 
-            commit.committer.date
-        )
-    }
-})
+await importCommits(db, commits);
