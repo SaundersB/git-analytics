398d33bafc8b12bfb7ca9e8e76b39cab344a61f2
Brandon Saunders <brandon.saunders@charlesriverlabs.com>
2022-12-08 15:22:45 -0800

    chore: we don't log around these parts

--- migrate.mjs
+++ migrate.mjs
@@ -2,9 +2,6 @@ import { openDb } from './db.mjs';
 
 async function main() {
   const db = await openDb();
-  db.on('trace', (data) => {
-    console.log(data);
-  })
   await db.migrate()
   await db.close();
 }
