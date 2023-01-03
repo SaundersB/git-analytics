a285f80cd57d5fa669fdf3051f79b80a5a940d4b
Tom Douglas <tomdoug@gmail.com>
2023-01-02 22:52:48 -0600

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -4,8 +4,8 @@ import { getCommitersByName } from './git-tool-providers/azure-dev-ops.mjs';
 const app = express();
 const port = 3000;
 
-app.get('/api/getCommitersByName', (req, res) => {
-    let commiters = getCommitersByName()
+app.get('/api/getCommitersByName', async (req, res) => {
+    let commiters = await getCommitersByName();
     res.send(commiters);
 });
 
